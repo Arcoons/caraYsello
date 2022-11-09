c262f360e662918d504e00d2160f69d9a7ec3c05
Christian <christianzaor1090@gmail.com>
2022-11-08 23:40:37 -0500

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 const juego = document.getElementById('moneda');
 juego.addEventListener('click',()=>{
       juego.classList.toggle('animate-heads')
-})
-      
+})      
 const Victoria= document.getElementById('ganar');
 const Derrota = document.getElementById('perder');
-let i = 0 ;
-let e = 0;
-let juegos = 0 ;
+const Apuesta = document.getElementById('apuesta');
+const Juegos = document.getElementById('juegos');
+let j = 0;
+let d = 0;
+let v = 0;
 let subtotal = 0;
 const cara = document.getElementById('cara_boton');
 cara.addEventListener('click', ()=>{
       let valor = document.getElementById('inp__').value;
       valor = parseFloat(valor)
       if(valor > 10000){
-            let rando =  Math.random()*2 + 1;
+            let rando =  Math.floor((Math.random() *2)+1);
             let moneda = rando.toPrecision(1);  
             if(moneda == 1){
-                  console.log("gano");
-                  e= e+1
-                  Victoria.innerText= e
-                  subtotal = subtotal + valor
-                  console.log(subtotal)
+                  v= v+1
+                  Victoria.innerText= v;
+                  j= j+1
+                  Juegos.innerText= j;
+                  subtotal = subtotal + valor;
+                  Apuesta.innerText=subtotal;
                   Swal.fire({title:"Felicidades ganaste"});
             }else if(moneda > 1){
-                  console.log("perdio");
-                  Derrota.innerHTML=i;  
-                  i = i+1   
-                  subtotal = subtotal - valor 
-                  console.log(subtotal)
+                  Derrota.innerText=d;
+                  d = d+1
+                  j=j+1
+                  Juegos.innerText= j;
+                  subtotal = subtotal-valor;
+                  Apuesta.innerText=subtotal;
                   Swal.fire({title:"Haz perdido, buena suerte en la siguiente"});
             }            
-            juegos = i + e;
+            juegos = d + v;
       
       }else{
             Swal.fire({title:"Ingresa una apuesta mayor a 10000"});
@@ -44,26 +47,30 @@ sello.addEventListener('click', ()=>{
       let valor = document.getElementById('inp__').value;
       valor = parseFloat(valor)
       if(valor > 10000){
-            let rando =  Math.random()*2 + 1;
+            let rando =  Math.floor((Math.random() *2)+1);
             let moneda = rando.toPrecision(1); 
             if(moneda==2){
                   console.log("gano");
-                  e= e+1
-                  Victoria.innerText= e
-                  subtotal = subtotal + valor
-                  console.log(subtotal)
+                  v= v+1
+                  Victoria.innerText= v
+                  subtotal = subtotal + valor;
+                  j=j+1
+                  Juegos.innerText= j;
+                  Apuesta.innerText=subtotal;
                   Swal.fire({title:"Felicidades ganaste"});
                   
             }else if(moneda <2 || moneda >2){
                   console.log("perdio");
-                  i =i+1
-                  Derrota.innerHTML=i;
-                  subtotal = subtotal - valor 
-                  console.log(subtotal)
+                  d =d+1
+                  Derrota.innerText=d;
+                  j=j+1
+                  Juegos.innerText= j;
+                  subtotal = subtotal - valor;
+                  Apuesta.innerText=subtotal;
                   Swal.fire({title:"Haz perdido, buena suerte en la siguiente"});
             } 
       
-            juegos = i + e;
+            juegos = d + v;
       }else{
             Swal.fire({title:"Ingresa una apuesta mayor a 10000"});
       }
@@ -74,11 +81,11 @@ sello.addEventListener('click', ()=>{
 const volver = document.getElementById('volver');
 volver.addEventListener('click', ()=>{
       if(subtotal <0){
-            alert(`Perdiste $ ${subtotal} y jugaste ${juegos} veces`);
+            alert(`Perdiste $ ${subtotal} y jugaste ${j} veces`);
             Swal.fire({title:"Gracias por jugar, vuelve pronto"});
            
       }else{
-            alert(`Ganaste $ ${subtotal} y jugaste ${juegos} veces`);
+            alert(`Ganaste $ ${subtotal} y jugaste ${j} veces`);
             Swal.fire({title:"Gracias por jugar, vuelve pronto"});
             
       }
